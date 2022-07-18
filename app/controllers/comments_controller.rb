a75c74b202f5f12c9686d17700e9bde0a0842a0b
Yu---ta <yutakoman1111@gmail.com>
2022-07-18 16:03:03 +0900

    first commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,27 @@
+class CommentsController < ApplicationController
+  def create
+    
+    @comment = Comment.new(comment_params)
+
+    if @comment.save
+      # @prototype = Prototype.find(params[:prototype_id])
+      @prototype = @comment.prototype
+      @comment = Comment.new
+      @comments = @prototype.comments
+      # redirect_to prototype_path(@comment.prototype)
+      render "prototypes/show"
+      
+    else
+      @prototype = Prototype.find(params[:prototype_id])
+      @comment = Comment.new
+      @comments = @prototype.comments
+      render "prototypes/show"
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content).merge(user_id: current_user.id, prototype_id: params[:prototype_id] )
+  end
+
+end
